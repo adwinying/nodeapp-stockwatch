8d8f16ff317f8a24a3bbf25804b7eb7c4b7c8fab
Adwin Ying <adwinying@gmail.com>
2017-06-10 21:55:08 +0900

    Implement client side web sockets

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 6000
+
+let stocks = ['AAPL', 'GOOG', 'FB']
 
 app.get('/', (req, res) => {
   res.json({ status: 'API online' })
 })
 
 io.on('connection', (socket) => {
-  console.log('user connected:', socket)
+  console.log('user connected')
+  socket.on('get_stock_list', () => {
+    socket.emit('stock_list', stocks)
+  })
+
+  socket.on('update_stock', (data) => {
+    stocks = data
+    io.emit('stock_list', data)
+  })
 })
 
 http.listen(port, () => {
